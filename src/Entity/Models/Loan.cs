0359689a4e44a09edfa409eac157f22ce66ddd3a
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2021-11-05 18:29:03 +0100

    Created loan model class

---
+++ src/Entity/Models/Loan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Models
+{
+    public class Loan
+    {
+        
+        public Guid Id { get; set; }
+
+        public int Amount { get; set; }
+
+        public Guid RequesterId { get; set; }
+        
+        public string Comment { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+
+
+    }
+}
+
+//requesterId - Guid //FK to the loan requester (the person who created the loan application)
+//comment - string // the requester comment
+//createdAt - Date
