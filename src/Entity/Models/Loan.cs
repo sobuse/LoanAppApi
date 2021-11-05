be72f4513e1dca04a4200631bd0b0da93a6753d1
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2021-11-05 18:39:27 +0100

    Deleted unwanted space

--- src/Entity/Models/Loan.cs
+++ src/Entity/Models/Loan.cs
@@ -8,22 +8,11 @@ namespace Entity.Models
 {
     public class Loan
     {
-        
         public Guid Id { get; set; }
-
         public int Amount { get; set; }
-
         public Guid RequesterId { get; set; }
-        
         public string Comment { get; set; }
-
         public DateTime CreatedAt { get; set; }
-
-
-
     }
 }
 
-//requesterId - Guid //FK to the loan requester (the person who created the loan application)
-//comment - string // the requester comment
-//createdAt - Date
