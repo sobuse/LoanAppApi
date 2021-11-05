154f44931e5f04bfb61f5a533eda00f5369cad95
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2021-11-05 21:36:13 +0100

    Added a comment

--- src/Entity/Models/LoanOfficer.cs
+++ src/Entity/Models/LoanOfficer.cs
@@ -18,6 +18,7 @@ namespace Entity.Models
         [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
         public string Fullname { get; set; }
 
+        // the request loan shows the status of the loan the borrower requested for, it could be pending | Appoved | declined
         public RequestLoan RequestLoan { get; set; }
     }
 }
