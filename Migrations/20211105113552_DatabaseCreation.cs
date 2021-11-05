a669c1343748796e7b94c2407fbd92022851a4f5
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2021-11-05 13:44:50 +0100

    made changes

---
+++ Migrations/20211105113552_DatabaseCreation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LoanAppApi.Migrations
+{
+    public partial class DatabaseCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Borrowers",
+                columns: table => new
+                {
+                    BorrowersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    LoanAmount = table.Column<int>(type: "int", nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BorrowedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MonthlyIncome = table.Column<int>(type: "int", nullable: false),
+                    BVN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LoanOfficerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Borrowers", x => x.BorrowersId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LoanOfficers",
+                columns: table => new
+                {
+                    LoanOfficerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Fullname = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    requestLoan = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LoanOfficers", x => x.LoanOfficerId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Borrowers");
+
+            migrationBuilder.DropTable(
+                name: "LoanOfficers");
+        }
+    }
+}
