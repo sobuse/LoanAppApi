18cce6bf01746c246f3d504c11e50eff340c52fc
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2021-11-12 06:03:25 +0100

    created IRepository in the contract project

---
+++ src/Contract/IRepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract
+{
+    public  interface IRepositoryBase<T>
+    {
+        IQueryable<T> FindAll(bool trackChanges);
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression,
+         bool trackChanges);
+
+    }
+}
