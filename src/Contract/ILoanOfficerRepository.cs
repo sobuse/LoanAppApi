a2104797dd94562adf919600707830438eec995b
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2021-11-12 08:51:20 +0100

    Interfaces Inherits from repositoryBase generic abstract class

---
+++ src/Contract/ILoanOfficerRepository.cs
@@ -0,0 +1,13 @@
+﻿using Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract
+{
+    public interface ILoanOfficerRepository : IRepositoryBase<LoanOfficer>
+    {
+    }
+}
