294639ffe6bc519df26986227298e5ae41f188cd
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2021-11-12 06:20:17 +0100

    Defined RepositoryBase  abstract class

---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,23 @@
+﻿using Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        public IQueryable<T> FindAll(bool trackChanges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
