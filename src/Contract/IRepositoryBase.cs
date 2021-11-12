5bbc57b7aba2a0cdc5481af1e7131cf5d0331921
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2021-11-12 08:54:01 +0100

    refactored repository interface

--- src/Contract/IRepositoryBase.cs
+++ src/Contract/IRepositoryBase.cs
@@ -9,9 +9,20 @@ namespace Contract
 {
     public  interface IRepositoryBase<T>
     {
-        IQueryable<T> FindAll(bool trackChanges);
-        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression,
-         bool trackChanges);
+        T Add(T obj);
+        public IEnumerable<T> AddRange(IEnumerable<T> obj);
+        void Insert(T entity);
+        Task<T> AddAsync(T obj);
+        IEnumerable<T> GetAll();
+        Task<IEnumerable<T>> GetAllAsync();
+        T GetById(object id);
+        Task<T> GetByIdAsync(object id);
+        T GetSingleByCondition(Expression<Func<T, bool>> predicate = null, Func<IQueryable, IOrderedQueryable> orderby = null, params string[] includeProperties);
+        IEnumerable<T> GetByCondition(Expression<Func<T, bool>> predicate = null, Func<IQueryable, IOrderedQueryable> orderby = null, int? skip = null, int? take = null, params string[] includeProperties);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate = null);
+        bool Any(Expression<Func<T, bool>> predicate = null);
+        (T, string) SoftDelete(Guid Id);
 
     }
 }
